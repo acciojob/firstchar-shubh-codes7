97cc788e5f9861bf8b80ea280b6a3a53cc19c4e7
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-07 18:38:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstChar(text) {
   // your code here
+	text = text.trim();
+	return text.length == 0 ? "" : text[0];
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+const text = prompt("Enter text:");
+alert(firstChar(text)); 
